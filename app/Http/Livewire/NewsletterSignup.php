0c70626de18921e0457ca6da9133f6cbc420b617
BRAD Technologies Dev <46785019+brad-tech-dev@users.noreply.github.com>
2022-10-19 22:40:45 +0300

    Created the newsletter signup form

---
+++ app/Http/Livewire/NewsletterSignup.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class NewsletterSignup extends Component
+{
+    public function render()
+    {
+        return view('livewire.newsletter-signup');
+    }
+}
