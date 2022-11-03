3c9a7138cc6e6a2bfdd4082c4614f9933e99f7d0
BRAD Technologies Dev <46785019+brad-tech-dev@users.noreply.github.com>
2022-11-03 12:27:00 +0300

    Removed the second cooperative word

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -8,14 +8,14 @@
                     Qui sommes-nous?
                 </h2>
                 <p class="mb-4">
-                    Ayant vu le jour sous l'initiative de l'opérateur économique Burundais, Ingénieur BIKORIMANA Innocent, la société Coopérative Gira Umutahe, CGU en sigle, est une société coopérative de type multifonctionnel tel que défini dans l'article 2 de la Loi n°1/12 du 28 juin 2017 régissant les Sociétés Coopératives au Burundi.
+                    Ayant vu le jour sous l'initiative de l'opérateur économique Burundais, Ingénieur BIKORIMANA Innocent, la société Coopérative Gira Umutahe, CGU en sigle, est une société de type multifonctionnel tel que défini dans l'article 2 de la Loi n°1/12 du 28 juin 2017 régissant les Sociétés Coopératives au Burundi.
                 </p>
                 <p>
                     Le siège de la CGU est situé au chef-lieu de la commune et province Bubanza et sa gestion quotidienne se conforme aux principes coopératifs universellement reconnus tel que l'adhésion volontaire et ouverte à tous ; le pouvoir démocratique exercé par les coopérateurs ; la participation économique des coopérateurs ; l'autonomie et l'indépendance de la société coopérative ; l'éducation, la formation et l'information ; l'inter coopération ou coopération entre les coopératives ; le dévouement à la communauté.
                 </p>
             </div>
-            <div class="grid grid-cols-2 gap-4 mt-8">
-                <img class="rounded-lg" src="{{ asset('images/burundi-flag-on-flagpole.jpg') }}" title="Photo par aboodi vesakaran: https://www.pexels.com/photo/burundi-national-flag-13913360/" alt="Burundi flag on flag pole">
+            <div class="grid grid-cols-1 gap-4 mt-8">
+                {{-- <img class="rounded-lg" src="{{ asset('images/burundi-flag-on-flagpole.jpg') }}" title="Photo par aboodi vesakaran: https://www.pexels.com/photo/burundi-national-flag-13913360/" alt="Burundi flag on flag pole"> --}}
                 <img class="mt-4 w-full lg:mt-10 rounded-lg" src="{{ asset('images/mpoiu.jpg') }}" alt="CGU Offices" title="Bikorimana Innocent" >
             </div>
         </div>
