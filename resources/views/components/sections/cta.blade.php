10f34a9df83c7fe50036a2cee61917c48d7588f7
BRAD Technologies Dev <46785019+brad-tech-dev@users.noreply.github.com>
2022-11-04 10:47:34 +0300

    Commented out links and added client's text

--- resources/views/components/sections/cta.blade.php
+++ resources/views/components/sections/cta.blade.php
@@ -1,15 +1,15 @@
 <section id="cta" class="bg-white dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div class="max-w-screen-lg text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 class="mb-4 text-4xl tracking-tight font-bold text-gray-900 dark:text-white">Powering innovation at <span
-                    class="font-extrabold">200,000+</span> companies worldwide</h2>
-            <p class="mb-4 font-light">Track work across the enterprise through an open, collaborative platform. Link
-                issues across Jira and ingest data from other software development tools, so your IT support and
-                operations teams have richer contextual information to rapidly respond to requests, incidents, and
-                changes.</p>
-            <p class="mb-4 font-medium">Deliver great service experiences fast - without the complexity of traditional
-                ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease.</p>
-            <a href="#"
+            {{-- <h2 class="mb-4 text-4xl tracking-tight font-bold text-gray-900 dark:text-white">Powering innovation at <span
+                    class="font-extrabold">200,000+</span> companies worldwide</h2> --}}
+            <p class="mb-4 font-light">
+                La CGU Burundi a mis à votre disposition une Plateforme qui vous offre la formation dont vous avez besoin pour réussir dans votre vie  entrepreneuriale et surtout surtout coopérative.
+            </p>
+            <p class="mb-4 font-medium">
+                Notre souhait est que tous les citoyens africains en général et particulièrement Burundais vivent une vie d'abondance et de prospérité à travers l'entrepreneuriat coopératif.
+            </p>
+            {{-- <a href="#"
                 class="inline-flex items-center font-medium text-primary-600 hover:text-primary-800 dark:text-primary-500 dark:hover:text-primary-700">
                 Learn more
                 <svg class="ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -17,7 +17,7 @@
                         d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
                         clip-rule="evenodd"></path>
                 </svg>
-            </a>
+            </a> --}}
         </div>
     </div>
 </section>
