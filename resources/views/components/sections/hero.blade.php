aa82195e1d92c8312930e96fbd8a7684c2587d5e
BRAD Technologies Dev <46785019+brad-tech-dev@users.noreply.github.com>
2023-02-02 12:36:49 +0300

    Reduced the size of greeting.jpeg

--- resources/views/components/sections/hero.blade.php
+++ resources/views/components/sections/hero.blade.php
@@ -1,5 +1,5 @@
 <section class="bg-white dark:bg-gray-900 bg-cover bg-center"
-style="background-image: url({{ asset('images/greeting.jpeg') }});">
+style="background-image: url({{ asset('images/greeting.jpg') }});">
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12 backdrop-contrast-100 backdrop-brightness-50">
 
         @livewire('announcement-badge', ['href' => route('blog.index'), 'title' => 'Nouveau', 'subject' => 'Nous avons une nouvelle publication'])
